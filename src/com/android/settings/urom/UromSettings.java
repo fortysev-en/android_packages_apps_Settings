790f3c4662df9a0ffda63242b73bcb1ca4015074
Pushkar Jadhao <pushkar.jadhao@gmail.com>
2017-01-07 18:57:52 +0530

    LightBar removed

--- src/com/android/settings/urom/UromSettings.java
+++ src/com/android/settings/urom/UromSettings.java
@@ -36,6 +36,7 @@ import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.urom.utils.HwScreenColors;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -45,8 +46,10 @@ public class UromSettings extends SettingsPreferenceFragment
     private static final String TAG = "UromSettings";
 
     //urom
-    private static final String COLOR_CALIBRATION_KEY = "color_calibration";
     private static final String DOZE_BRIGHTNESS_KEY = "doze_brightness";
+    private static final String LIGHTBAR_MODE_KEY = "lightbar_mode";
+    private static final String COLOR_CALIBRATION_KEY = "color_calibration";
+
     private static final String MAINKEYS_MUSIC_KEY = "mainkeys_music";
     private static final String MAINKEYS_MUSIC_PROPERTY = "persist.qemu.hw.mainkeys_music";
     private static final String CAMERAKEY_KEY = "camerakey";
@@ -130,10 +133,7 @@ public class UromSettings extends SettingsPreferenceFragment
         PreferenceCategory mCategory = null;
 
 	mCategory = (PreferenceCategory) findPreference("urom_shortcut_category");
-     if (!getResources().getBoolean(R.bool.config_urom_ambient)) {
-            mCategory.removePreference(findPreference(DOZE_BRIGHTNESS_KEY));
-     }
-    if (removePref("eu.chainfire.supersu"))
+	if (removePref("eu.chainfire.supersu"))
 	    mCategory.removePreference(findPreference("urom_shortcut_supersu_settings"));
 	if (removePref("com.lovejoy777.rroandlayersmanager"))
 	    mCategory.removePreference(findPreference("urom_shortcut_bitsyko_layers"));
@@ -143,6 +143,9 @@ public class UromSettings extends SettingsPreferenceFragment
 	    mCategory.removePreference(findPreference("urom_shortcut_wakeup_settings"));
 
 	mCategory = (PreferenceCategory) findPreference("urom_display_category");
+	if (!getResources().getBoolean(R.bool.config_urom_ambient)) {
+	    mCategory.removePreference(findPreference(DOZE_BRIGHTNESS_KEY));
+	}
 	if (!HwScreenColors.isSupported()) {
 	    mCategory.removePreference(findPreference(COLOR_CALIBRATION_KEY));
 	}
@@ -175,29 +178,28 @@ public class UromSettings extends SettingsPreferenceFragment
 	if (!getResources().getBoolean(R.bool.config_urom_speakerprox)) {
 	    mCategory.removePreference(mDialer);
 	}
-     if (!getResources().getBoolean(R.bool.config_urom_lockscreen_phone)) {
-         mCategory.removePreference(mLockscreenPhone);
-         }
-
+	if (!getResources().getBoolean(R.bool.config_urom_lockscreen_phone)) {
+	    mCategory.removePreference(mLockscreenPhone);
+	}
 	if (!getResources().getBoolean(R.bool.config_urom_jack_broken)) {
 	    mCategory.removePreference(mJackBroken);
 	}
-    }
 
-    //Remove all empty categories
-    List<Preference> prefCatForRemove = new ArrayList<Preference>();
-    PreferenceScreen prefScreen = getPreferenceScreen();
-    int prefCount = prefScreen.getPreferenceCount();
-    for(int i=0; i<prefCount; i++) {
-        Preference pref = prefScreen.getPreference(i);
-        if (pref instanceof PreferenceCategory
-                && ((PreferenceCategory)pref).getPreferenceCount() == 0) {
-            prefCatForRemove.add(pref);
-     }
-      }
-    for(Preference pref: prefCatForRemove) {
-        prefScreen.removePreference(pref);
+        //Remove all empty categories
+        List<Preference> prefCatForRemove = new ArrayList<Preference>();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int prefCount = prefScreen.getPreferenceCount();
+        for(int i=0; i<prefCount; i++) {
+            Preference pref = prefScreen.getPreference(i);
+            if (pref instanceof PreferenceCategory
+                    && ((PreferenceCategory)pref).getPreferenceCount() == 0) {
+                prefCatForRemove.add(pref);
+            }
         }
+        for(Preference pref: prefCatForRemove) {
+            prefScreen.removePreference(pref);
+        }
+    }
 
     private boolean removePref(String pkg) {
 	try {
